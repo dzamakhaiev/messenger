9f7084c715f30eae83a70cd6943023b26502144b
dzamakhaiev <dzamakhaiev@star.global>
2024-01-16 15:31:46 +0200

    update rest api and settings

--- server_side/app/rest_api.py
+++ server_side/app/rest_api.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from threading import Thread
+import requests
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse
 import settings
+
+TASK_MANAGER_URL = f'http://{settings.LOCAL_HOST}:{settings.TASK_API_PORT}{settings.TASK_RESOURCE}'
 
 
 class User(Resource):
@@ -18,7 +22,7 @@ class User(Resource):
         return 'Test DELETE method', 200
 
 
-class Messages(Resource):
+class Message(Resource):
 
     def get(self):
         return 'Test GET method', 200
@@ -27,6 +31,19 @@ class Messages(Resource):
         return 'Test PUT method', 201
 
     def post(self):
+        json_dict = request.json
+
+        if json_dict.get('message'):
+            sender_address = f"{request.environ['REMOTE_ADDR']}:{request.environ['REMOTE_PORT']}"
+            new_json_dict = {'json': json_dict, 'sender_address': sender_address}
+
+            try:
+                send_task = Thread(
+                    target=lambda: requests.post(TASK_MANAGER_URL, json=new_json_dict), daemon=True)
+                send_task.start()
+            except requests.exceptions.ConnectionError:
+                pass
+
         return 'Test POST method', 201
 
     def delete(self):
@@ -36,6 +53,6 @@ class Messages(Resource):
 if __name__ == '__main__':
     app = Flask(__name__)
     api = Api(app)
-    api.add_resource(Messages, "/messages")
-    api.add_resource(User, "/user")
-    app.run(host=settings.LOCAL_HOST, port=settings.REST_API_PORT, debug=True)
+    api.add_resource(Message, settings.MESSAGE_RESOURCE)
+    api.add_resource(User, settings.USER_RESOURCE)
+    app.run(host=settings.LOCAL_HOST, port=settings.REST_API_PORT, debug=True, use_reloader=False)
