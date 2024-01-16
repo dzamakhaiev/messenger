77fab87848140eea940748ee9f340f78a21b028f
dzamakhaiev <dzamakhaiev@star.global>
2024-01-16 18:00:42 +0200

    update task manager

--- server_side/app/task_manager.py
+++ server_side/app/task_manager.py
@@ -1,9 +1,13 @@
+import requests
+import settings
+
 from time import sleep
 from queue import Queue
 from threading import Thread
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
-import settings
+
+from server_side.database.db_handler import DatabaseHandler
 
 INITIAL_INTERVAL = 1
 MAX_INTERVAL = 60
@@ -14,7 +18,13 @@ queue = Queue()
 class Task(Resource):
 
     def post(self):
-        queue.put('Test item')
+        json_dict = request.json
+        if json_dict:
+
+            if json_dict.get('message'):
+                task = ('message', json_dict)
+                queue.put(task)
+
         return 'Test POST method', 201
 
 
@@ -24,6 +34,7 @@ class TaskManager:
         self.queue = queue
         self.interval = INITIAL_INTERVAL
         self.count = 0
+        self.db = DatabaseHandler()
 
     def dynamic_interval(self):
         sleep(self.interval)
@@ -35,6 +46,50 @@ class TaskManager:
             self.interval += 5
             self.count = 0
 
+    def reset_interval(self):
+        self.interval = INITIAL_INTERVAL
+        self.count = 0
+
+    def try_send_message(self, receiver_address, message, sender_id):
+        try:
+            requests.post(receiver_address, json={'message': message, 'sender_id': sender_id})
+            return True
+        except requests.exceptions.ConnectionError:
+            return False
+
+    def check_address_list(self, address_list):
+        active_address_list = [address for address, status in address_list if status == 'Active']
+        return active_address_list
+
+    def process_message_task(self, json_dict):
+        message = json_dict.get('message')
+        sender_id = json_dict.get('sender_id')
+        receiver_id = json_dict.get('receiver_id')
+        sender_address = json_dict.get('sender_address')
+
+        # Save or update existed sender address to db
+        self.db.insert_or_update_user_address(sender_id, sender_address)
+
+        # Get receiver address list from db
+        receiver_address_list = self.db.get_user_address(receiver_id)
+        if receiver_address_list:
+            active_address_list = self.check_address_list(receiver_address_list)
+
+        else:
+            # TODO some exception or return
+            active_address_list = []
+
+        # Try to send message to each active address
+        if active_address_list:
+            for receiver_address in active_address_list:
+                # TODO add db data parser
+                result = self.try_send_message(receiver_address, message, sender_id)
+                if not result:
+                    self.db.deactivate_user_address(user_id=receiver_id, user_address=receiver_address)
+
+        else:
+            pass
+
     def main_loop(self):
 
         while True:
@@ -42,7 +97,13 @@ class TaskManager:
                 self.dynamic_interval()
 
                 if not self.queue.empty():
-                    print('Queue is not empty.')
+                    print('Task is started.')
+                    task = self.queue.get()
+                    task_type, json_dict = task
+
+                    if task_type == 'message':
+                        self.process_message_task(json_dict)
+                        self.reset_interval()
 
             except KeyboardInterrupt:
                 break
@@ -51,7 +112,7 @@ class TaskManager:
 if __name__ == '__main__':
     app = Flask(__name__)
     api = Api(app)
-    api.add_resource(Task, "/task")
+    api.add_resource(Task, settings.TASK_RESOURCE)
 
     task_thread = Thread(daemon=True,
                          target=lambda: app.run(host=settings.LOCAL_HOST,
