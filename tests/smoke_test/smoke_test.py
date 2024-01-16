9c74cda7688d742edc8fcc9cd990d4b41f497a23
dzamakhaiev <dzamakhaiev@star.global>
2024-01-16 13:21:10 +0200

    update tests

--- tests/smoke_test/smoke_test.py
+++ tests/smoke_test/smoke_test.py
@@ -3,7 +3,10 @@ from unittest import TestCase, main
 
 LOCALHOST = 'http://127.0.0.1'
 LOCAL_PORT = '5000'
-LOCAL_URL = f'{LOCALHOST}:{LOCAL_PORT}'
+REST_API_URL = f'{LOCALHOST}:{LOCAL_PORT}'
+
+TASK_PORT = 5001
+TASK_URL = f'{LOCALHOST}:{TASK_PORT}'
 
 
 class SmokeTest(TestCase):
@@ -17,7 +20,7 @@ class SmokeTest(TestCase):
             with self.subTest(f'Test GET method for endpoint: {endpoint}'):
 
                 try:
-                    response = requests.get(LOCAL_URL + endpoint)
+                    response = requests.get(REST_API_URL + endpoint)
                     self.assertEqual(response.status_code, 200)
                 except requests.exceptions.ConnectionError:
                     self.fail('No connection to the server')
@@ -27,7 +30,7 @@ class SmokeTest(TestCase):
             with self.subTest(f'Test PUT method for endpoint: {endpoint}'):
 
                 try:
-                    response = requests.put(LOCAL_URL + endpoint)
+                    response = requests.put(REST_API_URL + endpoint)
                     self.assertEqual(response.status_code, 201)
                 except requests.exceptions.ConnectionError:
                     self.fail('No connection to the server')
@@ -37,7 +40,7 @@ class SmokeTest(TestCase):
             with self.subTest(f'Test POST method for endpoint: {endpoint}'):
 
                 try:
-                    response = requests.post(LOCAL_URL + endpoint)
+                    response = requests.post(REST_API_URL + endpoint)
                     self.assertEqual(response.status_code, 201)
                 except requests.exceptions.ConnectionError:
                     self.fail('No connection to the server')
@@ -47,10 +50,17 @@ class SmokeTest(TestCase):
             with self.subTest(f'Test DELETE method for endpoint: {endpoint}'):
 
                 try:
-                    response = requests.delete(LOCAL_URL + endpoint)
+                    response = requests.delete(REST_API_URL + endpoint)
                     self.assertEqual(response.status_code, 200)
                 except requests.exceptions.ConnectionError:
                     self.fail('No connection to the server')
+
+    def test_task_post_method(self):
+        try:
+            response = requests.post(TASK_URL + '/task')
+            self.assertEqual(response.status_code, 201)
+        except requests.exceptions.ConnectionError:
+            self.fail('No connection to the server')
 
 
 if __name__ == '__main__':
