e71b1a1710e7ae30d4ef6097cf77905be11ab34f
dzamakhaiev <dzamakhaiev@star.global>
2024-01-17 12:34:19 +0200

    add client sender

---
+++ client_side/backend/sender.py
@@ -0,0 +1,14 @@
+import requests
+import settings
+
+
+class ClientSender:
+
+    def __init__(self):
+        self.listener_url = f'http://{settings.LISTENER_HOST}:{settings.LISTENER_PORT}'
+
+    def send_request(self, json_dict=None):
+        try:
+            response = requests.post(url=settings.SERVER_URL, json=json_dict)
+        except requests.exceptions.ConnectionError:
+            pass
