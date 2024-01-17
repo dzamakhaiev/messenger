11a47506f25efac115a4a6a9209e54d8c31fb8a9
dzamakhaiev <dzamakhaiev@star.global>
2024-01-17 13:13:23 +0200

    update gui interface

--- client_side/gui/interface.py
+++ client_side/gui/interface.py
@@ -1,11 +1,11 @@
 import settings
 from tkinter import Tk, ttk, Frame, Text
-from tkinter import CENTER, NO
+from tkinter import LEFT, TOP, BOTTOM, CENTER, BOTH
 
 main_window = Tk()
-channel_menu = Frame(main_window, bg='grey')
-channel_table = ttk.Treeview(master=main_window)
-log_widget = Text(master=main_window, height=10, bg='green')
+side_menu = Frame(main_window, bg='grey')
+conversation_widget = Text(master=main_window, height=10, bg='blue')
+type_widget = Text(master=main_window, height=10, bg='green')
 
 
 if __name__ == "__main__":
@@ -15,15 +15,22 @@ if __name__ == "__main__":
     main_window.geometry(f'{settings.WINDOW_RESOLUTION}+{settings.WINDOW_POSITION_SHIFT}')
     main_window.configure(bg=settings.WINDOW_BACKGROUND)
 
-    # Create channel's menu
-    channel_menu.pack_propagate(True)
-    channel_menu.pack(fill='both', side='left')
+    # Side menu
+    side_menu.pack_propagate(True)
+    side_menu.pack(fill=BOTH, side=LEFT)
 
-    # Add quit button
-    button_quit = ttk.Button(channel_menu, text='Quit', command=main_window.destroy)
+    # Quit button
+    button_quit = ttk.Button(side_menu, text='Quit', command=main_window.destroy)
     button_quit.pack(padx=5, pady=10)
 
-    # Add text widget for logs
-    log_widget.pack(side='bottom', fill='x', expand=True)
+    # Conversation text field
+    conversation_widget.pack(side=TOP, fill=BOTH, expand=True)
+
+    # Send button
+    button_quit = ttk.Button(main_window, text='Send')
+    button_quit.pack(side=BOTTOM, padx=5, pady=10)
+
+    # Type text field
+    type_widget.pack(side=BOTTOM, fill=BOTH, expand=True)
 
     main_window.mainloop()
