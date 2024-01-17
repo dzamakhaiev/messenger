280b4fe69fec6186a95c3f4eb5dcb983f8cc01d4
dzamakhaiev <dzamakhaiev@star.global>
2024-01-17 12:09:24 +0200

    add helper

---
+++ helpers/network.py
@@ -0,0 +1,10 @@
+import socket
+
+
+def find_free_port(port=5000):
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        if s.connect_ex(('localhost', port)) == 0:
+            return find_free_port(port=port + 1)
+        else:
+            return port
