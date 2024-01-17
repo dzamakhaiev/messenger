d019b173b461f2d19628f4c815bd8f694d32e780
dzamakhaiev <dzamakhaiev@star.global>
2024-01-17 13:29:06 +0200

    change start port

--- helpers/network.py
+++ helpers/network.py
@@ -1,7 +1,7 @@
 import socket
 
 
-def find_free_port(port=5000):
+def find_free_port(port=5005):
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         if s.connect_ex(('localhost', port)) == 0:
