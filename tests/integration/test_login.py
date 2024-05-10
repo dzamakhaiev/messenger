b57fa8f33ab67dbb908e4aeea2ee9166aa5faa7d
dzamakhaiev <dzamakhaiev@star.global>
2024-05-10 16:26:04 +0300

    add new test

--- tests/integration/test_login.py
+++ tests/integration/test_login.py
@@ -1,3 +1,4 @@
+import copy
 import unittest
 import requests
 import test_data
@@ -21,10 +22,32 @@ class LoginTest(unittest.TestCase):
         else:
             self.assertEqual(200, response.status_code, msg=response.text)
 
+            # Test response data
             user_id = response.json()['user_id']
             self.assertEqual(test_data.USER_ID, user_id, f'Incorrect user_id: {user_id}')
             session_id = response.json()['session_id']
             self.assertIsInstance(session_id, str, f'Unexpected session_id data type: {session_id}')
+
+    def test_incorrect_login(self):
+        correct_json = {'username': test_data.USERNAME, 'password': test_data.PASSWORD, 'user_address': 'some_ip'}
+
+        for field in ('username', 'password', 'username and password'):
+
+            with self.subTest(f'Login with incorrect {field}'):
+                incorrect_json = copy.copy(correct_json)
+
+                if field.count(' ') > 0:
+                    incorrect_json['username'] = 'incorrect'
+                    incorrect_json['password'] = 'incorrect'
+                else:
+                    incorrect_json[field] = 'incorrect'
+
+                response = post_request(self.login_url, incorrect_json)
+                if isinstance(response, requests.ConnectionError):
+                    self.fail(response)
+                else:
+                    self.assertEqual(401, response.status_code, msg=response.text)
+                    self.assertEqual('Incorrect username or password.', response.text)
 
 
 if __name__ == '__main__':
