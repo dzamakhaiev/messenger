a5b4adb8f82d767d4c60071f96e5d9620171493a
dzamakhaiev <dzamakhaiev@star.global>
2024-05-14 16:52:08 +0300

    refactor tests

--- tests/integration/test_find_user.py
+++ tests/integration/test_find_user.py
@@ -1,50 +1,31 @@
 import unittest
-import requests
 import test_data
-from helpers.network import post_request
+from tests.test_framework import TestFramework
 
 
-class UsersTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        url = test_data.SERVER_URL
-        cls.login_url = url + test_data.LOGIN
-        cls.users_url = url + test_data.USERS
+class UsersTest(TestFramework):
 
     def setUp(self):
-        correct_json = {'username': test_data.USERNAME, 'password': test_data.PASSWORD, 'user_address': 'some_ip'}
-        response = post_request(self.login_url, correct_json)
+        json_login = {'username': test_data.USERNAME, 'password': test_data.PASSWORD, 'user_address': 'some_ip'}
+        response = self.log_in(json_login)
         self.session_id = response.json()['session_id']
+        self.correct_json = {'username': test_data.USERNAME, 'session_id': self.session_id}
 
     def test_get_user_id_positive(self):
-        correct_json = {'username': test_data.USERNAME, 'session_id': self.session_id}
-        response = post_request(self.users_url, correct_json)
-
-        if isinstance(response, requests.ConnectionError):
-            self.fail(response)
-
-        else:
-            self.assertEqual(200, response.status_code, msg=response.text)
-            user_id = response.json()['user_id']
-            self.assertEqual(test_data.USER_ID, user_id, f'Incorrect user_id: {user_id}')
+        response = self.get_user_id(self.correct_json)
+        self.assertEqual(200, response.status_code, msg=response.text)
+        user_id = response.json()['user_id']
+        self.assertEqual(test_data.USER_ID, user_id, f'Incorrect user_id: {user_id}')
 
     def test_get_no_user(self):
         incorrect_json = {'username': 'some user', 'session_id': self.session_id}
-        response = post_request(self.users_url, incorrect_json)
-
-        if isinstance(response, requests.ConnectionError):
-            self.fail(response)
-        else:
-            self.assertEqual(404, response.status_code, msg=response.text)
+        response = self.get_user_id(incorrect_json)
+        self.assertEqual(404, response.status_code, msg=response.text)
 
     def test_validation_error(self):
         incorrect_json = {'session_id': self.session_id}
-        response = post_request(self.users_url, incorrect_json)
-
-        if isinstance(response, requests.ConnectionError):
-            self.fail(response)
-        else:
-            self.assertEqual(400, response.status_code, msg=response.text)
+        response = self.get_user_id(incorrect_json)
+        self.assertEqual(400, response.status_code, msg=response.text)
 
 
 if __name__ == '__main__':
