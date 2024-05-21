47b5a5c3672c4de3b7399514ec113605a8a65253
dzamakhaiev <dzamakhaiev@star.global>
2024-05-21 13:36:41 +0300

    update test suite

--- tests/integration/test_get_user.py
+++ tests/integration/test_get_user.py
@@ -18,13 +18,15 @@ class UsersTest(TestFramework):
         user_id = response.json()['user_id']
         self.assertEqual(test_data.USER_ID, user_id, f'Incorrect user_id: {user_id}')
 
-    def test_get_no_user(self):
-        incorrect_json = corrupt_json_field(self.correct_json, 'username')
-        response = self.get_user_id(incorrect_json)
-        self.assertEqual(404, response.status_code, msg=response.text)
-
     def test_validation_error(self):
-        for field, code in (('session_id', 401), ('request', 400)):
+        for field, code in (('session_id', 401), ('request', 400), ('username', 400)):
+            with self.subTest(f'Get user with no "{field}" field.'):
+                incorrect_json = remove_json_field(self.correct_json, field)
+                response = self.get_user_id(incorrect_json)
+                self.assertEqual(code, response.status_code, msg=response.text)
+
+    def test_incorrect_data(self):
+        for field, code in (('session_id', 401), ('request', 400), ('username', 404)):
             with self.subTest(f'Get user with incorrect "{field}" field.'):
                 incorrect_json = corrupt_json_field(self.correct_json, field)
                 response = self.get_user_id(incorrect_json)
