4205cb001e13142be2e0cb5189153d171b300abf
dzamakhaiev <dzamakhaiev@star.global>
2024-05-23 15:55:10 +0300

    add logging for listener and service

--- server_side/app/listener.py
+++ server_side/app/listener.py
@@ -16,29 +16,39 @@ from server_side.database.db_handler import HDDDatabaseHandler, RAMDatabaseHandl
 from server_side.app.service import Service
 from server_side.app.models import UserLogin, User, Message
 
-
-logging.basicConfig(format=settings.LOG_FORMAT)
+# Set up listener and its logger
 app = Flask(__name__)
+listener_logger = logging.getLogger(__name__)
+listener_logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(settings.LOG_FORMAT)
+handler = logging.FileHandler(f"../logs/listener.log")
+handler.setFormatter(formatter)
+listener_logger.addHandler(handler)
 
 # Set up DB handlers
 hdd_db_handler = HDDDatabaseHandler()
 ram_db_handler = RAMDatabaseHandler()
 hdd_db_handler.create_all_tables()
 ram_db_handler.create_all_tables()
-service = Service(hdd_db_handler, ram_db_handler, app.logger)
+service = Service(hdd_db_handler, ram_db_handler)
 
 
 def create_user(request_json: dict):
     try:
         user = User(**request_json)
     except ValidationError as e:
+        listener_logger.error(f'User validation caused error: {e}')
         return settings.VALIDATION_ERROR, 400
 
     username = hdd_db_handler.get_user(username=user.username)
     if username:
+        listener_logger.error(f'Username "{username}" already exists.')
         return 'Username already exists.', 400
+
     else:
         user_id = service.create_user(user)
+        listener_logger.info(f'User "{username}" created with id {user_id}')
         return jsonify({'user_id': user_id}), 201
 
 
@@ -47,12 +57,14 @@ def get_user(request_json: dict):
     if username := request_json.get('username'):
         user_id = service.get_user_id_by_username(username)
     else:
+        listener_logger.error('Field "username" is missing.')
         return settings.VALIDATION_ERROR, 400
 
     if user_id:
         return jsonify({'user_id': user_id})
     else:
-        return f'User "{username}" not found', 404
+        listener_logger.error(f'User "{username}" not found.')
+        return f'User "{username}" not found.', 404
 
 
 @app.route(routes.LOGIN, methods=['POST'])
@@ -60,28 +72,30 @@ def login():
     try:
         user = UserLogin(**request.json)
     except ValidationError as e:
+        listener_logger.error(f'Login validation caused error: {e}')
         return settings.VALIDATION_ERROR, 400
 
     exp_password = hdd_db_handler.get_user_password(user.username)
     if exp_password and exp_password == user.password:
         user_id = service.get_user_id_by_username(user.username)
-        app.logger.info(f'User "{user.username}" with user id "{user_id}" logged in.')
-
         ram_db_handler.insert_username(user_id, user.username)  # store it in ram for further checks
         session_id = service.get_or_create_user_session(user_id)
+
         service.store_user_address_and_session(user_id, session_id, user.user_address)
+        listener_logger.info(f'User id "{user_id}" logged in with session id "{session_id}".')
 
         # Check not received messages for current user
         messages = service.get_messages(user_id)
         if messages:
             address_list = service.get_user_address(user_id)
-            app.logger.info(f'Send messages to "{user_id}" user id after log in.')
+            listener_logger.info(f'Send messages to "{user_id}" user id after log in.')
             service.send_messages_by_list(address_list, messages)
 
         return jsonify({'msg': 'Login successful.', 'user_id': user_id, 'session_id': session_id})
 
     else:
-        return f'Incorrect username or password.', 401
+        listener_logger.error('Incorrect username or password.')
+        return 'Incorrect username or password.', 401
 
 
 @app.route(f'{routes.USERS}', methods=['POST'])
@@ -97,6 +111,7 @@ def users():
         return get_user(request.json)
 
     else:
+        listener_logger.error('Validation error for "/users" request.')
         return settings.VALIDATION_ERROR, 400
 
 
@@ -105,27 +120,30 @@ def process_messages():
     try:
         msg = Message(**request.json)
     except ValidationError as e:
-        app.logger.error(e)
+        listener_logger.error(f'Message validation caused error: {e}')
         return settings.VALIDATION_ERROR, 400
 
     if not service.check_user_id(msg.receiver_id):
-        app.logger.error(f'User id "{msg.receiver_id}" not found.')
+        listener_logger.error(f'User id "{msg.receiver_id}" not found.')
         return settings.VALIDATION_ERROR, 400
 
     username = service.get_username_by_user_id(msg.sender_id)
     session_id = service.get_session_id(msg.sender_id)
 
     if msg.session_id == session_id and msg.sender_username == username:
-        address_list = service.get_user_address(msg.receiver_id)
-        app.logger.info(f'Send message to "{msg.receiver_id}" user id.')
+        listener_logger.info(f'Send message to "{msg.receiver_id}" user id.')
 
+        address_list = service.get_user_address(msg.receiver_id)
         msg_state = service.send_message_by_list(address_list, request.json)
         state = 'received' if msg_state else 'sent'
         return settings.MESSAGE_STATE.format(state), 200
 
     else:
+        listener_logger.error('Invalid username or session id.')
         return settings.NOT_AUTHORIZED, 401
 
 
 if __name__ == '__main__':
+    listener_logger.info('Listener started.')
     app.run(host=settings.REST_API_HOST, port=settings.REST_API_PORT, debug=True, use_reloader=False)
+    listener_logger.info('Listener Ended.')
--- server_side/app/service.py
+++ server_side/app/service.py
@@ -1,18 +1,28 @@
 import uuid
 import socket
+import logging
 import requests
+import settings
 from urllib.parse import urlparse
 
 
 LOCAL_IP = socket.gethostbyname(socket.gethostname())
 
+service_logger = logging.getLogger(__name__)
+service_logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(settings.LOG_FORMAT)
+handler = logging.FileHandler(f"../logs/service.log")
+handler.setFormatter(formatter)
+service_logger.addHandler(handler)
+
 
 class Service:
 
-    def __init__(self, hdd_db_handler, ram_db_handler, logger):
+    def __init__(self, hdd_db_handler, ram_db_handler):
+        service_logger.info('Service logger started.')
         self.hdd_db_handler = hdd_db_handler
         self.ram_db_handler = ram_db_handler
-        self.logger = logger
 
     @staticmethod
     def check_url(url: str):
@@ -25,14 +35,16 @@ class Service:
     def send_message(self, url, msg_json):
         try:
             url = self.check_url(url)
-            self.logger.info(f'Sent message to url: {url}')
+            service_logger.info(f'Sent message to url: {url}')
             response = requests.post(url, json=msg_json, timeout=5)
-            self.logger.info(f'Message sent with status code: {response.status_code}')
+            service_logger.info(f'Message sent with status code: {response.status_code}')
             return response
+
         except requests.exceptions.ConnectionError as e:
-            self.logger.error(f'Message sent with error: {e}')
+            service_logger.error(f'Response with error: {e}')
 
     def send_message_by_list(self, address_list, msg_json):
+        service_logger.info('Send message by address list.')
         message_received = False
 
         for user_address in address_list:
@@ -45,10 +57,12 @@ class Service:
                 print(e)
 
         if not message_received:
+            service_logger.info('Message not sent. Store it to DB in RAM.')
             self.store_message_to_db(msg_json)
         return message_received
 
     def send_messages_by_list(self, address_list, messages):
+        service_logger.info('Send messages by address list.')
         messages_to_delete = []
 
         for message in messages:
@@ -58,15 +72,20 @@ class Service:
             msg_received = self.send_message_by_list(address_list, msg_json)
 
             if msg_received:
+                service_logger.info('Message deleted from RAM DB.')
                 messages_to_delete.append(msg_id)
 
         messages_to_delete = ','.join([str(msg) for msg in messages_to_delete])
         self.ram_db_handler.delete_messages(messages_to_delete)
 
     def create_user(self, user):
+        service_logger.info('Create new user.')
+
         self.hdd_db_handler.insert_user(user.username, user.phone_number, user.password)
         user_id = self.hdd_db_handler.get_user_id(user.username)
         self.ram_db_handler.insert_username(user_id, user.username)
+
+        service_logger.info(f'User with {user.username} created: user id "{user_id}".')
         return user_id
 
     def store_message_to_db(self, msg_json):
@@ -76,19 +95,20 @@ class Service:
                                            msg_json.get('message'))
 
     def store_user_address_and_session(self, user_id, session_id, user_address):
-        # Store user data in both DBs for back up
+        service_logger.info('Store user session and user addresses in HDD and RAM DBs.')
         self.ram_db_handler.insert_session_id(user_id, session_id)
         self.hdd_db_handler.insert_session_id(user_id, session_id)
         self.ram_db_handler.insert_user_address(user_id, user_address)
         self.hdd_db_handler.insert_user_address(user_id, user_address)
 
     def get_or_create_user_session(self, user_id):
+        service_logger.info(f'Get or create session for user id "{user_id}".')
         session_id = self.get_session_id(user_id)
+        if not session_id:
+            session_id = str(uuid.uuid4())
 
-        if session_id:
-            return session_id
-        else:
-            return str(uuid.uuid4())
+        service_logger.debug(f'Session id: "{session_id}".')
+        return session_id
 
     def get_session_id(self, user_id):
         session_id = self.ram_db_handler.get_user_session(user_id)
@@ -98,6 +118,7 @@ class Service:
             return self.hdd_db_handler.get_user_session(user_id)
 
     def get_user_id_by_username(self, username):
+        service_logger.debug(f'Get user id for username "{username}".')
         user_id = self.ram_db_handler.get_user_id(username)
         if not user_id:
             user_id = self.hdd_db_handler.get_user_id(username)
@@ -105,6 +126,7 @@ class Service:
         return user_id
 
     def get_username_by_user_id(self, user_id):
+        service_logger.debug(f'Get username for user id "{user_id}".')
         username = self.ram_db_handler.get_username(user_id)
         if not username:
             username = self.hdd_db_handler.get_username(user_id)
@@ -115,6 +137,7 @@ class Service:
             return ''
 
     def get_user_address(self, user_id):
+        service_logger.debug(f'Get user address for user id "{user_id}".')
         address_list = self.ram_db_handler.get_user_address(user_id)
         if not address_list:
             address_list = self.hdd_db_handler.get_user_address(user_id)
@@ -122,10 +145,12 @@ class Service:
         return address_list
 
     def get_messages(self, user_id):
+        service_logger.debug(f'Get messages for user id "{user_id}".')
         messages = self.ram_db_handler.get_user_messages(user_id)
         return messages
 
     def check_session_exists(self, session_id):
+        service_logger.info(f'Check session id "{session_id}".')
         session_id = self.ram_db_handler.get_session(session_id)
         if not session_id:
             session_id = self.hdd_db_handler.get_session(session_id)
@@ -136,6 +161,7 @@ class Service:
             return False
 
     def check_user_id(self, user_id):
+        service_logger.info(f'Check user id "{user_id}".')
         result = self.ram_db_handler.get_user(user_id=user_id)
         if not result:
             result = self.hdd_db_handler.get_user(user_id=user_id)
@@ -144,3 +170,6 @@ class Service:
             return True
         else:
             return False
+
+    def __del__(self):
+        service_logger.info('Service logger ended.')
