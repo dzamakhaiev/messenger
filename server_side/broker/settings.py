f571b8cde972a1581b8354fb9aceabb4c6c4b237
dzamakhaiev <dzamakhaiev@star.global>
2024-05-25 19:30:53 +0300

    add settings

--- server_side/broker/settings.py
+++ server_side/broker/settings.py
@@ -1,4 +1,4 @@
-LOG_FORMAT = "%(asctime)s %(levelname)s %(module)s: %(message)s"
+CONNECT_URI = 'amqp://guest:guest@localhost:5672/%2F'
 MQ_HOST = 'localhost'
 MQ_PORT = 5672
 MQ_DELIVERY_MODE = 2  # Persistent mode saves messages on HDD in case of crash
