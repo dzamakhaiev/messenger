7d284795f94d0e766763a434ada8813c1b246e0b
dzamakhaiev <dzamakhaiev@star.global>
2024-05-26 18:02:04 +0300

    update rabbitmq port

--- server_side/broker/settings.py
+++ server_side/broker/settings.py
@@ -1,4 +1,4 @@
 MQ_PORT = 5672
-MQ_HOST = '192.168.50.122'
+MQ_HOST = '192.168.50.100'
 CONNECT_URI = f'amqp://guest:guest@{MQ_HOST}:{MQ_PORT}/%2F'
 MQ_DELIVERY_MODE = 2  # Persistent mode saves messages on HDD in case of crash
