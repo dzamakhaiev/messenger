aa08f309f2b0a51c77e573752c30ce4d6607197c
dzamakhaiev <dzamakhaiev@star.global>
2024-05-30 16:13:51 +0300

    add new request types

--- helpers/network.py
+++ helpers/network.py
@@ -4,7 +4,6 @@ from random import randint
 
 
 def find_free_port(port=5005):
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         if s.connect_ex(('localhost', port)) == 0:
             return find_free_port(port=port + randint(0, 995))
@@ -12,9 +11,33 @@ def find_free_port(port=5005):
             return port
 
 
+def get_request(url, params=None):
+    try:
+        response = requests.get(url, params=params)
+        return response
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+        return e
+
+
 def post_request(url, json_dict=None):
     try:
         response = requests.post(url, json=json_dict)
+        return response
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+        return e
+
+
+def put_request(url, json_dict=None):
+    try:
+        response = requests.put(url, json=json_dict)
+        return response
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+        return e
+
+
+def patch_request(url, json_dict=None):
+    try:
+        response = requests.patch(url, json=json_dict)
         return response
     except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
         return e
