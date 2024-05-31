c950cb126233bc55da60c8cbb7f55acf01f692ea
dzamakhaiev <dzamakhaiev@star.global>
2024-05-31 17:35:59 +0300

    fix user id

--- tests/integration/test_messages.py
+++ tests/integration/test_messages.py
@@ -14,7 +14,7 @@ class MessagesTest(TestFramework):
         self.user = self.create_new_user()
         self.log_in_with_listener_url(user=self.user, listener_port=find_free_port())
         self.msg_json = {'message': 'test', 'sender_id': self.user.user_id, 'sender_username': self.user.username,
-                         'receiver_id': test_data.ANOTHER_USER_ID, 'session_id': self.user.session_id,
+                         'receiver_id': None, 'session_id': self.user.session_id,
                          'send_date': datetime.now().strftime(test_data.DATETIME_FORMAT)}
 
     def test_send_message_to_offline_user(self):
@@ -85,6 +85,9 @@ class MessagesTest(TestFramework):
         self.assertEqual(new_queue.qsize(), 1, 'No message in queue.')
 
     def test_validation_error(self):
+        new_user = self.create_new_user()
+        self.msg_json['receiver_id'] = new_user.user_id
+
         for field in ['message', 'sender_id', 'sender_username', 'receiver_id', 'session_id', 'send_date']:
 
             with self.subTest(f'Send message with no "{field}" field.'):
@@ -93,6 +96,9 @@ class MessagesTest(TestFramework):
                 self.assertEqual(400, response.status_code, msg=response.text)
 
     def test_data_error(self):
+        new_user = self.create_new_user()
+        self.msg_json['receiver_id'] = new_user.user_id
+
         for field, code in [('sender_id', 400), ('sender_username', 401), ('receiver_id', 400), ('session_id', 401)]:
 
             with self.subTest(f'Send message with incorrect "{field}" field.'):
