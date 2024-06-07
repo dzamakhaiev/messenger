e89ee48091056e95ec49242a1b9949dcf16d90cc
dzamakhaiev <dzamakhaiev@star.global>
2024-06-07 12:22:56 +0300

    remove helpers

--- client_side/backend/settings.py
+++ client_side/backend/settings.py
@@ -1,4 +1,14 @@
-from helpers.network import find_free_port
+import socket
+from random import randint
+
+
+def find_free_port(port=5005):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        if s.connect_ex(('localhost', port)) == 0:
+            return find_free_port(port=port + randint(0, 995))
+        else:
+            return port
+
 
 LISTENER_HOST = '0.0.0.0'
 LISTENER_PORT = find_free_port()
