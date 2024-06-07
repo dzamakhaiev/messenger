48be71e0f8417175410c71d8b1af2e040bd8dbdf
dzamakhaiev <dzamakhaiev@star.global>
2024-06-07 12:23:15 +0300

    remove tests

--- tests/settings.py
+++
@@ -1,8 +0,0 @@
-REST_API_HOST = '192.168.50.100'
-REST_API_PORT = 5000
-SERVER_URL = f'http://{REST_API_HOST}:{REST_API_PORT}'
-
-API = '/api'
-LOGIN = API + '/login/'
-USERS = API + '/users/'
-MESSAGES = API + '/messages/'
