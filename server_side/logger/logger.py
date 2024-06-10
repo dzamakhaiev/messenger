0a9b4f0a2de528eb2e1de08227c5c61b74b87ee5
dzamakhaiev <dzamakhaiev@star.global>
2024-06-10 15:55:50 +0300

    remove loki from messenger

--- server_side/logger/logger.py
+++ server_side/logger/logger.py
@@ -1,7 +1,5 @@
 import os
 import logging
-import requests
-from datetime import datetime
 from server_side.logger import settings
 
 
@@ -24,39 +22,14 @@ class Logger:
         stdout_handler.setFormatter(formatter)
         self.logger.addHandler(stdout_handler)
 
-    def push_log_to_loki(self, log_row, level):
-        self.logger.debug('Push message to Loki container.')
-        timestamp = str(int(datetime.now().timestamp() * 1e9))
-
-        log_json = {
-            'streams': [{
-                'stream': {
-                    'service': self.logger_name,
-                    'level': level},
-                'values': [[timestamp, log_row]]}]
-        }
-
-        try:
-            response = requests.post(url=settings.LOKI_URL+settings.LOKI_PUSH, json=log_json)
-            if response.status_code != 204:
-                self.logger.error(f'Cannot push log to Loki container: {response.text}')
-            else:
-                self.logger.debug('Message pushed.')
-
-        except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
-            self.logger.critical(f'Cannot connect to Loki container: {e}')
-
     def error(self, msg):
         self.logger.error(msg)
-        self.push_log_to_loki(msg, 'error')
 
     def info(self, msg):
         self.logger.info(msg)
-        self.push_log_to_loki(msg, 'info')
 
     def debug(self, msg):
         self.logger.debug(msg)
-        self.push_log_to_loki(msg, 'debug')
 
 
 if __name__ == '__main__':
