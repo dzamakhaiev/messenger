9c12a7adf88085c95b463c0221fc52ee4100e65e
dzamakhaiev <dzamakhaiev@star.global>
2024-07-18 15:38:06 +0300

    fix pylint errors for sender.py

--- server_side/app/sender.py
+++ server_side/app/sender.py
@@ -1,3 +1,7 @@
+"""
+This module listens a RabbitMQ queues and send messages to defined ip address.
+"""
+
 import os
 import sys
 import json
@@ -27,6 +31,9 @@ service = Service(hdd_db_handler, ram_db_handler, msg_broker)
 
 
 def process_message(channel, method, properties, body):
+    """
+    This function process message from RabbitMQ queue and send message to ip address in separated thread.
+    """
     sender_logger.info('Message received.')
     channel.basic_ack(delivery_tag=method.delivery_tag)
     message = body.decode()
@@ -36,14 +43,20 @@ def process_message(channel, method, properties, body):
     msg_json = message.get('msg_json')
     sender_logger.debug(f'Message to send:\n{msg_json}To user address list:\n{address_list}')
 
-    thread = Thread(target=service.send_message_by_list, args=(address_list, msg_json), daemon=True)
+    thread = Thread(target=service.send_message_by_list,
+                    args=(address_list, msg_json), daemon=True)
     sender_logger.debug(f'Thread "{thread.name}" created.')
     thread.start()
     sender_logger.debug(f'Thread "{thread.name}" started.')
 
 
 def process_login(channel, method, properties, body):
+    """
+    This function process message from RabbitMQ queue and check all not send messages
+    for defined user and try it send one more time after user logged in.
+    """
     sender_logger.info('Login event received.')
+
     channel.basic_ack(delivery_tag=method.delivery_tag)
     login_msg = body.decode()
     login_msg = json.loads(login_msg)
@@ -51,9 +64,11 @@ def process_login(channel, method, properties, body):
 
     messages = service.get_messages(login_msg['user_id'])
     address_list = [login_msg['user_address']]
-    sender_logger.debug(f'Messages to send after log in:\n{messages}\nTo user address list:\n{address_list}')
+    sender_logger.debug(f'Messages to send after log in:\n{messages}\n'
+                        f'To user address list:\n{address_list}')
 
-    thread = Thread(target=service.send_messages_by_list, args=(address_list, messages), daemon=True)
+    thread = Thread(target=service.send_messages_by_list,
+                    args=(address_list, messages), daemon=True)
     sender_logger.debug(f'Thread "{thread.name}" created.')
     thread.start()
     sender_logger.debug(f'Thread "{thread.name}" started.')
@@ -68,16 +83,23 @@ if __name__ == '__main__':
 
         try:
             sender_logger.info('Sender logger started.')
-            queue_dict = {settings.MQ_MSG_QUEUE_NAME: process_message, settings.MQ_LOGIN_QUEUE_NAME: process_login}
+            queue_dict = {settings.MQ_MSG_QUEUE_NAME: process_message,
+                          settings.MQ_LOGIN_QUEUE_NAME: process_login}
             connection = msg_broker.connect_and_consume_from_multiple_queues(queue_dict)
             connection.ioloop.start()
 
         except AMQPConnectionError as e:
             sender_logger.error(f'Connection to RabbitMQ lost: {e}')
 
-        except (KeyboardInterrupt, Exception) as e:
+        except KeyboardInterrupt:
             sender_logger.info('Sender logger ended.')
             connection.close()
             connection.ioloop.stop()
-            quit(e)
+            sys.exit(0)
+
+        except Exception as e:
+            sender_logger.error(e)
+            connection.close()
+            connection.ioloop.stop()
+            sys.exit(1)
 
