088fecd51850565153cb05a303f8ba958d77b5c8
dzamakhaiev <dzamakhaiev@star.global>
2024-07-25 14:53:28 +0300

    update containers statuses

--- scripts/wait_for_all_services.py
+++ scripts/wait_for_all_services.py
@@ -1,3 +1,7 @@
+"""
+That module checks all containers for 'ready to work' state.
+"""
+from time import sleep
 import add_messenger_to_sys_path
 import docker
 from docker.models.containers import Container
@@ -6,6 +10,16 @@ from logger.logger import Logger
 
 
 listener_logger = Logger('docker_handler')
+TIMEOUT = 60
+READY_TO_WORK_MARKERS = {
+    'nginx-ci': ['start worker processes', 'start worker process'],
+    'rabbitmq-ci': ['Ready to start client connection listeners', 'Time to start RabbitMQ:1'],
+    'postgres-ci': ['PostgreSQL init process complete; ready for start up.',
+                    'database system is ready to accept connections'],
+    'listener-ci': ['PostgreSQL connection established.', 'SQLite in-memory connection opened.',
+                    'RabbitMQ connection established.', 'Service logger started.'],
+    'sender-ci': ['Sender logger started.', 'Service logger started.', 'RabbitMQ connection established.',
+                  'SQLite in-memory connection opened.', 'PostgreSQL connection established.']}
 
 
 def is_docker_running():
@@ -37,11 +51,40 @@ def get_containers_logs(containers: list[Container]):
     containers_logs = {}
 
     for container in containers:
-        containers_logs[container.name] = str(container.logs(), encoding='utf-8', errors='ignore')
+        containers_logs[container.name] = str(container.logs(), encoding='utf-8', errors='ignore').split('\n')
 
     return containers_logs
+
+
+def check_containers_logs_for_markers(containers_logs: dict):
+    containers_statuses = {}
+    for container_name, container_logs in containers_logs.items():
+
+        # Copy markers to remove found markers from log
+        container_markers = READY_TO_WORK_MARKERS[container_name][:]
+
+        # Check all markers starting from the end of log
+        for log_line in container_logs[::-1]:
+            preliminary_flags = []
+
+            if container_markers:
+                for log_marker in container_markers:
+
+                    if log_marker in log_line:
+                        preliminary_flags.append(True)
+                        container_markers.remove(log_marker)
+
+            else:
+                if all(preliminary_flags):
+                    containers_statuses[container_name] = True
+                else:
+                    containers_statuses[container_name] = False
+
+    return containers_statuses
 
 
 if __name__ == '__main__':
     running_containers = get_all_containers()
     running_containers_logs = get_containers_logs(running_containers)
+    statuses = check_containers_logs_for_markers(running_containers_logs)
+    print(statuses)
