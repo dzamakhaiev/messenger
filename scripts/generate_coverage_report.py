713a448b51d418bd3d30ccc84b9a5804b17148e3
dzamakhaiev <dzamakhaiev@star.global>
2024-07-29 15:44:36 +0300

    fix working directory for coverage script

--- scripts/generate_coverage_report.py
+++ scripts/generate_coverage_report.py
@@ -1,6 +1,11 @@
+from os import path
+from os import chdir
 from coverage import Coverage
 
-cov_obj = Coverage(data_file=r'../.coverage', config_file=r'../.coveragerc')
+cov_obj = Coverage(data_file=path.abspath(r'../.coverage'),
+                   config_file=path.abspath(r'../.coveragerc'))
+chdir('..')
+
 cov_obj.start()
 cov_obj.stop()
 cov_obj.report()
