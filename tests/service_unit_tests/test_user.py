80cde4845c7e3e1ed629656ee8e3e0185f895f3b
dzamakhaiev <dzamakhaiev@star.global>
2024-07-29 21:09:47 +0300

    add new unit tests

--- tests/service_unit_tests/test_user.py
+++ tests/service_unit_tests/test_user.py
@@ -15,5 +15,57 @@ class TestUser(TestCase):
         self.user = User(**test_data.USER_CREATE_JSON)
 
     def test_user_create(self):
+        # Preconditions setup
+        self.service.hdd_db_handler.get_user_id.return_value = test_data.USER_ID
+
+        # Run target method with mocked handlers
         user_id = self.service.create_user(self.user)
         self.assertEqual(user_id, test_data.USER_ID)
+
+    def test_get_user_id(self):
+        # First case: get data from RAM database
+        self.service.ram_db_handler.get_user_id.return_value = test_data.USER_ID
+        user_id = self.service.get_user_id_by_username(test_data.USERNAME)
+        self.assertEqual(user_id, test_data.USER_ID)
+
+        # Second case: get data from HDD database
+        self.service.ram_db_handler.get_user_id.return_value = None
+        self.service.hdd_db_handler.get_user_id.return_value = test_data.USER_ID
+        user_id = self.service.get_user_id_by_username(test_data.USERNAME)
+        self.assertEqual(user_id, test_data.USER_ID)
+
+        # Third case: no user data in both databases
+        self.service.ram_db_handler.get_user_id.return_value = None
+        self.service.hdd_db_handler.get_user_id.return_value = None
+        user_id = self.service.get_user_id_by_username(test_data.USERNAME)
+        self.assertTrue(user_id is None)
+
+    def test_check_user_id(self):
+        # First case: get data from RAM database
+        self.service.ram_db_handler.get_user.return_value = test_data.USER_ID
+        user_id = self.service.check_user_id(test_data.USER_ID)
+        self.assertEqual(user_id, test_data.USER_ID)
+
+        # Second case: get data from HDD database
+        self.service.ram_db_handler.get_user.return_value = None
+        self.service.hdd_db_handler.get_user.return_value = test_data.USER_ID
+        user_id = self.service.check_user_id(test_data.USER_ID)
+        self.assertEqual(user_id, test_data.USER_ID)
+
+        # Third case: no user data in both databases
+        self.service.ram_db_handler.get_user.return_value = None
+        self.service.hdd_db_handler.get_user.return_value = None
+        user_id = self.service.check_user_id(test_data.USER_ID)
+        self.assertTrue(user_id is False)
+
+    def test_user_delete(self):
+        # First case: user exists in RAM database
+        self.service.ram_db_handler.get_user.return_value = test_data.USER_ID
+        user_deleted = self.service.delete_user(test_data.USER_ID)
+        self.assertTrue(user_deleted)
+
+        # Second case: user does exist in both databases
+        self.service.ram_db_handler.get_user.return_value = None
+        self.service.hdd_db_handler.get_user.return_value = None
+        user_deleted = self.service.delete_user(test_data.USER_ID)
+        self.assertFalse(user_deleted)
