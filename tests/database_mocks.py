6b7a96b89e85d63f94606f5752be72436caf52e6
dzamakhaiev <dzamakhaiev@star.global>
2024-07-29 12:21:55 +0300

    start unit tests

---
+++ tests/database_mocks.py
@@ -0,0 +1,19 @@
+from unittest.mock import Mock
+from server_side.database.sqlite_handler import RAMDatabaseHandler
+from server_side.database.postgres_handler import PostgresHandler
+from server_side.broker.mq_handler import RabbitMQHandler
+
+
+def get_hdd_db_handler():
+    hdd_db_handler = Mock(spec=PostgresHandler)
+    return hdd_db_handler
+
+
+def get_ram_db_handler():
+    ram_db_handler = Mock(spec=RAMDatabaseHandler)
+    return ram_db_handler
+
+
+def get_mq_handler():
+    mq_handler = Mock(spec=RabbitMQHandler)
+    return mq_handler
