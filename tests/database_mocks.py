7325b947f49224a8b680f60f2faf56f04151dae5
dzamakhaiev <dzamakhaiev@star.global>
2024-07-29 18:12:02 +0300

    add new unit test

--- tests/database_mocks.py
+++ tests/database_mocks.py
@@ -1,4 +1,5 @@
 from unittest.mock import Mock
+from tests import test_data
 from server_side.database.sqlite_handler import RAMDatabaseHandler
 from server_side.database.postgres_handler import PostgresHandler
 from server_side.broker.mq_handler import RabbitMQHandler
@@ -6,11 +7,20 @@ from server_side.broker.mq_handler import RabbitMQHandler
 
 def get_hdd_db_handler():
     hdd_db_handler = Mock(spec=PostgresHandler)
+
+    # User methods
+    hdd_db_handler.insert_user.return_value = None
+    hdd_db_handler.get_user_id.return_value = test_data.USER_ID
+
     return hdd_db_handler
 
 
 def get_ram_db_handler():
     ram_db_handler = Mock(spec=RAMDatabaseHandler)
+
+    # User methods
+    ram_db_handler.insert_username.return_value = None
+
     return ram_db_handler
 
 
--- tests/service_unit_tests/test_user.py
+++ tests/service_unit_tests/test_user.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from tests.database_mocks import get_hdd_db_handler, get_ram_db_handler, get_mq_handler
 from server_side.app.service import Service
+from server_side.app.models import User
+from tests import test_data
 
 
 class TestUser(TestCase):
@@ -10,5 +12,8 @@ class TestUser(TestCase):
                                ram_db_handler=get_ram_db_handler(),
                                mq_handler=get_mq_handler())
 
-    def test_user(self):
-        self.assertTrue(True)
+        self.user = User(**test_data.USER_CREATE_JSON)
+
+    def test_user_create(self):
+        user_id = self.service.create_user(self.user)
+        self.assertEqual(user_id, test_data.USER_ID)
