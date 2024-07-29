460b0c64da27b59b23c0ef0b196693d28e97f106
dzamakhaiev <dzamakhaiev@star.global>
2024-07-29 21:09:38 +0300

    refactor test data and mocks

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,2 +1,3 @@
 USER_ID = 1
-USER_CREATE_JSON = {'username': 'user', 'phone_number': '1234567890', 'password': 'qwerty'}
+USERNAME = 'username'
+USER_CREATE_JSON = {'username': 'username', 'phone_number': '1234567890', 'password': 'qwerty'}
