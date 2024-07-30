22311f0fc28ecd00410e5debe5557712f0a8e501
dzamakhaiev <dzamakhaiev@star.global>
2024-07-30 14:59:58 +0300

    update address

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,8 +5,8 @@ MESSAGE_ID = 1
 PASSWORD = 'qwerty'
 USERNAME = 'username'
 USER_TOKEN = 'some_token'
-USER_ADDRESS = 'some_ip_address'
 USER_PUBLIC_KEY = 'some_public_key'
+USER_ADDRESS = f'https://{'192.168.0.1'}:{5000}'
 USER_CREATE_JSON = {'username': 'username', 'phone_number': '1234567890', 'password': PASSWORD}
 USER_MESSAGE_JSON = {'message': 'test', 'sender_id': USER_ID, 'sender_username': USERNAME,
                      'receiver_id': 2, 'send_date': datetime.now()}
