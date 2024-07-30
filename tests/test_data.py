0094a2ae96519f8137805a960e731284b5aed0de
dzamakhaiev <dzamakhaiev@star.global>
2024-07-30 14:16:37 +0300

    add message dict

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,10 @@
 USER_ID = 1
+MESSAGE_ID = 1
 PASSWORD = 'qwerty'
 USERNAME = 'username'
 USER_TOKEN = 'some_token'
 USER_ADDRESS = 'some_ip_address'
 USER_PUBLIC_KEY = 'some_public_key'
-USER_CREATE_JSON = {'username': 'username', 'phone_number': '1234567890', 'password': 'qwerty'}
+USER_CREATE_JSON = {'username': 'username', 'phone_number': '1234567890', 'password': PASSWORD}
+USER_MESSAGE_JSON = {'message': 'test', 'sender_id': USER_ID, 'sender_username': USERNAME,
+                     'receiver_id': 2, 'send_date': '2000-01-01 00:00:00'}
