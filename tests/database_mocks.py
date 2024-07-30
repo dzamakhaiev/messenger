03c42a51145de5354219e1ce9f75394e37a23cbb
dzamakhaiev <dzamakhaiev@star.global>
2024-07-30 11:33:14 +0300

    add return type

--- tests/database_mocks.py
+++ tests/database_mocks.py
@@ -4,16 +4,16 @@ from server_side.database.postgres_handler import PostgresHandler
 from server_side.broker.mq_handler import RabbitMQHandler
 
 
-def get_hdd_db_handler():
+def get_hdd_db_handler() -> Mock:
     hdd_db_handler = Mock(spec=PostgresHandler)
     return hdd_db_handler
 
 
-def get_ram_db_handler():
+def get_ram_db_handler() -> Mock:
     ram_db_handler = Mock(spec=RAMDatabaseHandler)
     return ram_db_handler
 
 
-def get_mq_handler():
+def get_mq_handler() -> Mock:
     mq_handler = Mock(spec=RabbitMQHandler)
     return mq_handler
