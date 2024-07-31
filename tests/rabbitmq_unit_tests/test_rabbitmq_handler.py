cbcc102b056a6728b2db1fb58b18ebeda17536cc
dzamakhaiev <dzamakhaiev@star.global>
2024-07-31 17:42:43 +0300

    add new tests refactor

--- tests/rabbitmq_unit_tests/test_rabbitmq_handler.py
+++ tests/rabbitmq_unit_tests/test_rabbitmq_handler.py
@@ -1,13 +1,19 @@
+from time import sleep
 from unittest import TestCase
 from pika import BlockingConnection
 from pika.adapters.blocking_connection import BlockingChannel
 from server_side.broker.mq_handler import RabbitMQHandler
 
 
+WAIT_MESSAGE_TIME = 0.3
+
+
 class TestUser(TestCase):
 
     def setUp(self):
         self.mq_handler = RabbitMQHandler()
+        self.exchange_name = 'TestExchange'
+        self.queue_name = 'TestQueue'
 
     def test_reconnect(self):
         self.mq_handler.reconnect()
@@ -15,49 +21,64 @@ class TestUser(TestCase):
         self.assertTrue(isinstance(self.mq_handler.channel, BlockingChannel))
 
     def test_create_exchange(self):
-        exchange_name = 'TestExchange'
-        exchange_confirm = self.mq_handler.create_exchange(exchange_name=exchange_name)
+        exchange_confirm = self.mq_handler.create_exchange(exchange_name=self.exchange_name)
         self.assertEqual('Exchange.DeclareOk', exchange_confirm.method.NAME)
 
     def test_create_and_bind_queue(self):
-        exchange_name = 'TestExchange'
-        self.mq_handler.create_exchange(exchange_name=exchange_name)
-
-        queue_name = 'TestQueue'
-        queue, bind = self.mq_handler.create_and_bind_queue(queue_name=queue_name,
-                                                            exchange_name=exchange_name)
+        self.mq_handler.create_exchange(exchange_name=self.exchange_name)
+        queue, bind = self.mq_handler.create_and_bind_queue(queue_name=self.queue_name,
+                                                            exchange_name=self.exchange_name)
 
         self.assertEqual('Queue.DeclareOk', queue.method.NAME)
         self.assertEqual('Queue.BindOk', bind.method.NAME)
 
     def test_send_message(self):
         # Preconditions: test exchange and test queue
-        exchange_name = 'TestExchange'
-        queue_name = 'TestQueue'
-        self.mq_handler.create_exchange(exchange_name=exchange_name)
-        self.mq_handler.create_and_bind_queue(queue_name=queue_name, exchange_name=exchange_name)
+        self.mq_handler.create_exchange(exchange_name=self.exchange_name)
+        self.mq_handler.create_and_bind_queue(queue_name=self.queue_name,
+                                              exchange_name=self.exchange_name)
 
         # Put message to queue
         message = 'test'
         self.mq_handler.send_message(
-            exchange_name=exchange_name, queue_name=queue_name, body=message)
+            exchange_name=self.exchange_name, queue_name=self.queue_name, body=message)
 
         # Get message from queue
-        _, _, body = self.mq_handler.channel.basic_get(queue=queue_name, auto_ack=True)
+        _, _, body = self.mq_handler.channel.basic_get(queue=self.queue_name, auto_ack=True)
         self.assertEqual(message, body.decode())
 
     def test_receive_message(self):
         # Preconditions: test exchange and test queue
-        exchange_name = 'TestExchange'
-        queue_name = 'TestQueue'
-        self.mq_handler.create_exchange(exchange_name=exchange_name)
-        self.mq_handler.create_and_bind_queue(queue_name=queue_name, exchange_name=exchange_name)
+        self.mq_handler.create_exchange(exchange_name=self.exchange_name)
+        self.mq_handler.create_and_bind_queue(queue_name=self.queue_name,
+                                              exchange_name=self.exchange_name)
 
         # Put message to queue
         message = 'test'
-        self.mq_handler.send_message(
-            exchange_name=exchange_name, queue_name=queue_name, body=message)
+        self.mq_handler.send_message(exchange_name=self.exchange_name, queue_name=self.queue_name,
+                                     body=message)
 
         # Get message from queue
-        body = self.mq_handler.receive_message(queue_name=queue_name)
+        sleep(WAIT_MESSAGE_TIME)
+        body = self.mq_handler.receive_message(queue_name=self.queue_name)
         self.assertEqual(message, body)
+
+    def test_get_queue_len(self):
+        # Preconditions: test exchange and test queue
+        self.mq_handler.create_exchange(exchange_name=self.exchange_name)
+        self.mq_handler.create_and_bind_queue(queue_name=self.queue_name,
+                                              exchange_name=self.exchange_name)
+
+        # Put message to queue
+        message = 'test'
+        self.mq_handler.send_message(exchange_name=self.exchange_name, queue_name=self.queue_name,
+                                     body=message)
+
+        # Check queue len
+        sleep(WAIT_MESSAGE_TIME)
+        queue_len = self.mq_handler.get_queue_len(queue_name=self.queue_name)
+        self.assertEqual(queue_len, 1)
+
+    def tearDown(self):
+        self.mq_handler.channel.queue_delete(queue=self.queue_name)
+        self.mq_handler.channel.exchange_delete(exchange=self.exchange_name)
