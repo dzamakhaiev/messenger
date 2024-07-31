8e7706e3e91a2581ddc4c801d8fb5f55425134c1
dzamakhaiev <dzamakhaiev@star.global>
2024-07-31 17:31:28 +0300

    add new tests

--- tests/rabbitmq_unit_tests/test_rabbitmq_handler.py
+++ tests/rabbitmq_unit_tests/test_rabbitmq_handler.py
@@ -2,8 +2,6 @@ from unittest import TestCase
 from pika import BlockingConnection
 from pika.adapters.blocking_connection import BlockingChannel
 from server_side.broker.mq_handler import RabbitMQHandler
-from server_side.app import settings
-from tests import test_data
 
 
 class TestUser(TestCase):
@@ -31,3 +29,35 @@ class TestUser(TestCase):
 
         self.assertEqual('Queue.DeclareOk', queue.method.NAME)
         self.assertEqual('Queue.BindOk', bind.method.NAME)
+
+    def test_send_message(self):
+        # Preconditions: test exchange and test queue
+        exchange_name = 'TestExchange'
+        queue_name = 'TestQueue'
+        self.mq_handler.create_exchange(exchange_name=exchange_name)
+        self.mq_handler.create_and_bind_queue(queue_name=queue_name, exchange_name=exchange_name)
+
+        # Put message to queue
+        message = 'test'
+        self.mq_handler.send_message(
+            exchange_name=exchange_name, queue_name=queue_name, body=message)
+
+        # Get message from queue
+        _, _, body = self.mq_handler.channel.basic_get(queue=queue_name, auto_ack=True)
+        self.assertEqual(message, body.decode())
+
+    def test_receive_message(self):
+        # Preconditions: test exchange and test queue
+        exchange_name = 'TestExchange'
+        queue_name = 'TestQueue'
+        self.mq_handler.create_exchange(exchange_name=exchange_name)
+        self.mq_handler.create_and_bind_queue(queue_name=queue_name, exchange_name=exchange_name)
+
+        # Put message to queue
+        message = 'test'
+        self.mq_handler.send_message(
+            exchange_name=exchange_name, queue_name=queue_name, body=message)
+
+        # Get message from queue
+        body = self.mq_handler.receive_message(queue_name=queue_name)
+        self.assertEqual(message, body)
