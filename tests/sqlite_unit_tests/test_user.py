509785d258c53f54b853ee144a1ad509de9afd24
dzamakhaiev <dzamakhaiev@star.global>
2024-07-31 14:55:55 +0300

    update tests

--- tests/sqlite_unit_tests/test_user.py
+++ tests/sqlite_unit_tests/test_user.py
@@ -1,8 +1,11 @@
 import sqlite3
 from unittest import TestCase
 from server_side.database.sqlite_handler import RAMDatabaseHandler
-
 from tests import test_data
+
+
+RAM_TABLES = {'user_address': 'user_address', 'usernames': 'usernames',
+              'tokens': 'tokens', 'public_keys': 'public_keys'}
 
 
 class TestUser(TestCase):
@@ -13,43 +16,43 @@ class TestUser(TestCase):
     def test_create_user_address_table(self):
         self.ram_db_handler.create_user_address_table()
 
-        table_query = "SELECT * FROM user_address"
+        table_query = f"SELECT * FROM {RAM_TABLES['user_address']}"
         try:
             self.ram_db_handler.cursor.execute(table_query)
             self.assertTrue(True)
 
         except sqlite3.DatabaseError as e:
-            self.assertFalse('no such table: user_address' in e.args[0])
+            self.assertFalse(f'no such table: {RAM_TABLES['user_address']}' in e.args[0])
 
     def test_create_usernames_table(self):
         self.ram_db_handler.create_usernames_table()
 
-        table_query = "SELECT * FROM usernames"
+        table_query = f"SELECT * FROM {RAM_TABLES['usernames']}"
         try:
             self.ram_db_handler.cursor.execute(table_query)
             self.assertTrue(True)
 
         except sqlite3.DatabaseError as e:
-            self.assertFalse('no such table: usernames' in e.args[0])
+            self.assertFalse(f"no such table: {RAM_TABLES['usernames']}" in e.args[0])
 
     def test_create_tokens_table(self):
         self.ram_db_handler.create_tokens_table()
 
-        table_query = "SELECT * FROM tokens"
+        table_query = f"SELECT * FROM {RAM_TABLES['tokens']}"
         try:
             self.ram_db_handler.cursor.execute(table_query)
             self.assertTrue(True)
 
         except sqlite3.DatabaseError as e:
-            self.assertFalse('no such table: tokens' in e.args[0])
+            self.assertFalse(f"no such table: {RAM_TABLES['tokens']}" in e.args[0])
 
     def test_create_public_keys_table(self):
         self.ram_db_handler.create_public_keys_table()
 
-        table_query = "SELECT * FROM public_keys"
+        table_query = f"SELECT * FROM {RAM_TABLES['public_keys']}"
         try:
             self.ram_db_handler.cursor.execute(table_query)
             self.assertTrue(True)
 
         except sqlite3.DatabaseError as e:
-            self.assertFalse('no such table: public_keys' in e.args[0])
+            self.assertFalse(f"no such table: {RAM_TABLES['public_keys']}" in e.args[0])
