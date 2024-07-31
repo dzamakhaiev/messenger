86214dfa759353ec77ff0b5b48fc775a308322d9
dzamakhaiev <dzamakhaiev@star.global>
2024-07-31 18:16:58 +0300

    add new test

--- tests/rabbitmq_unit_tests/test_rabbitmq_handler.py
+++ tests/rabbitmq_unit_tests/test_rabbitmq_handler.py
@@ -1,4 +1,6 @@
+import json
 from time import sleep
+from threading import Thread
 from unittest import TestCase
 from pika import BlockingConnection
 from pika.adapters.blocking_connection import BlockingChannel
@@ -38,14 +40,19 @@ class TestUser(TestCase):
         self.mq_handler.create_and_bind_queue(queue_name=self.queue_name,
                                               exchange_name=self.exchange_name)
 
-        # Put message to queue
+        # First case: put string message to queue
         message = 'test'
         self.mq_handler.send_message(
             exchange_name=self.exchange_name, queue_name=self.queue_name, body=message)
-
-        # Get message from queue
         _, _, body = self.mq_handler.channel.basic_get(queue=self.queue_name, auto_ack=True)
         self.assertEqual(message, body.decode())
+
+        # Second case: put dict message to queue
+        message = {'message': 'test'}
+        self.mq_handler.send_message(
+            exchange_name=self.exchange_name, queue_name=self.queue_name, body=message)
+        _, _, body = self.mq_handler.channel.basic_get(queue=self.queue_name, auto_ack=True)
+        self.assertEqual(message, json.loads(body.decode()))
 
     def test_receive_message(self):
         # Preconditions: test exchange and test queue
@@ -78,6 +85,40 @@ class TestUser(TestCase):
         sleep(WAIT_MESSAGE_TIME)
         queue_len = self.mq_handler.get_queue_len(queue_name=self.queue_name)
         self.assertEqual(queue_len, 1)
+
+    def test_connect_and_consume_from_multiple_queues(self):
+        # Preconditions
+        self.mq_handler.create_exchange(exchange_name=self.exchange_name)
+        self.mq_handler.create_and_bind_queue(queue_name=self.queue_name,
+                                              exchange_name=self.exchange_name)
+        external_results = {}
+
+        def run_io_loop_in_thread():
+            def process_message_callback(channel, method, properties, body):
+                # Create callback function to process messages
+                channel.basic_ack(delivery_tag=method.delivery_tag)
+                message = body.decode()
+                message = json.loads(message)
+                external_results['message'] = message
+
+            # Run io loop for defined queue with callback function
+            queue_dict = {self.queue_name: process_message_callback}
+            connection = self.mq_handler.connect_and_consume_from_multiple_queues(queue_dict)
+            connection.ioloop.start()
+
+        # Run infinite loop in daemon thread
+        thread = Thread(target=run_io_loop_in_thread, daemon=True)
+        thread.start()
+
+        # Send message
+        message = {'message': 'test message for consumer'}
+        self.mq_handler.send_message(exchange_name=self.exchange_name, queue_name=self.queue_name,
+                                     body=message)
+
+        # Check message received
+        sleep(WAIT_MESSAGE_TIME)
+        self.assertEqual(len(external_results), 1)
+        self.assertEqual(external_results.get('message'), message)
 
     def tearDown(self):
         self.mq_handler.channel.queue_delete(queue=self.queue_name)
