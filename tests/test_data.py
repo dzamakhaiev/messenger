54d2b2935ea741820768870543252fdfc410741b
dzamakhaiev <dzamakhaiev@star.global>
2024-08-01 21:17:28 +0300

    add new test data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 USER_ID = 1
+USER_ID_2 = 2
 MESSAGE_ID = 1
 PASSWORD = 'qwerty'
 USERNAME = 'username'
+USERNAME_2 = 'username_2'
 USER_TOKEN = 'some_token'
+PHONE_NUMBER = '123456789'
+PHONE_NUMBER_2 = '1234567890'
 USER_PUBLIC_KEY = 'some_public_key'
 USER_ADDRESS = f'https://{'192.168.0.1'}:{5000}'
 USER_CREATE_JSON = {'username': 'username', 'phone_number': '1234567890', 'password': PASSWORD}
