89711fbe7067f7c8cb2a3088035356b8ca14424f
dzamakhaiev <dzamakhaiev@star.global>
2024-08-03 19:51:52 +0300

    relocate coverage report to scripts dir

--- generate_xml_report.py
+++
@@ -1,9 +0,0 @@
-from os import path
-from coverage import Coverage
-
-data_file = path.abspath(r'.coverage')
-cov_obj = Coverage(data_file=data_file)
-
-if path.isfile(data_file):
-    cov_obj.load()
-    cov_obj.xml_report(skip_empty=True)
