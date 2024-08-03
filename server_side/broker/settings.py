c4ec8c3890f0fb9f2687dc8b69244e8ffef07e3d
dzamakhaiev <dzamakhaiev@star.global>
2024-08-03 18:19:12 +0300

    debug get_hostname import

--- server_side/broker/settings.py
+++ server_side/broker/settings.py
@@ -1,5 +1,5 @@
 import os
-from scripts.get_container_host import get_container_host
+from scripts.get_container_hostname import get_container_host
 
 INSIDE_DOCKER = int(os.environ.get('RUN_INSIDE_DOCKER', 0))
 CI_RUN = int(os.environ.get('CI_RUN', 0))
