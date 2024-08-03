cb8929dd208d2de7c293d51422cbd56b0f0a5212
dzamakhaiev <dzamakhaiev@star.global>
2024-08-03 18:06:08 +0300

    debug get_hostname import

--- scripts/add_messenger_to_sys_path.py
+++ scripts/add_messenger_to_sys_path.py
@@ -10,7 +10,3 @@ current_file = os.path.realpath(__file__)
 current_dir = os.path.dirname(current_file)
 repo_dir = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.insert(0, repo_dir)
-
-# Add messenger/scripts to sys.path
-scripts_dir = os.path.abspath(os.path.join(repo_dir, 'scripts'))
-sys.path.insert(0, scripts_dir)
