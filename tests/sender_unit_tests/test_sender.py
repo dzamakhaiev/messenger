3b96c1111cf283f3b6f0ee59405306bea9735f0e
dzamakhaiev <dzamakhaiev@star.global>
2024-08-03 16:11:18 +0300

    add test for sender

--- tests/sender_unit_tests/test_sender.py
+++ tests/sender_unit_tests/test_sender.py
@@ -20,6 +20,15 @@ class TestSender(TestCase):
                 'message': test_data.USER_MESSAGE_JSON.get('message'),
                 'send_date': test_data.USER_MESSAGE_JSON.get('send_date')}
 
+    @staticmethod
+    def create_message_from_db_like():
+        return (test_data.MESSAGE_ID,
+                test_data.USER_MESSAGE_JSON.get('sender_id'),
+                test_data.USER_MESSAGE_JSON.get('receiver_id'),
+                test_data.USER_MESSAGE_JSON.get('sender_username'),
+                test_data.USER_MESSAGE_JSON.get('message'),
+                test_data.USER_MESSAGE_JSON.get('send_date'))
+
     def test_process_message(self):
 
         # Mock RabbitMQ objects
@@ -52,6 +61,46 @@ class TestSender(TestCase):
         try:
             body = b'some incorrect data for json loader'
             process_message(channel=channel, method=method, properties=properties, body=body)
+            self.fail('')
+        except ValueError as e:
+            self.assertTrue(isinstance(e, (BaseException,)))
+
+    @mock.patch('server_side.app.service.Service.get_messages')
+    def test_process_login(self, mock_get_messages):
+
+        # Mock RabbitMQ objects
+        channel = mock.Mock(spec=Channel)
+        method = mock.Mock(spec=Basic.Deliver)
+        properties = mock.Mock(spec=BasicProperties)
+        method.delivery_tag = ''
+
+        # Mocking HDD DB response
+        expected_messages = [self.create_message_from_db_like()]
+        mock_get_messages.return_value = expected_messages
+
+        # Prepare queue login message
+        queue_dict = {'user_id': test_data.USER_ID, 'user_address': test_data.USER_ADDRESS}
+        queue_json = json.dumps(queue_dict)
+        body = queue_json.encode()
+
+        # First case: correct data from RabbitMQ
+        try:
+            process_login(channel=channel, method=method, properties=properties, body=body)
+            mock_get_messages.assert_called_once_with(test_data.USER_ID)
+        except (ValueError, AttributeError) as e:
+            self.fail(e)
+
+        # Second case: incorrect data type for decoding from bytes to string
+        try:
+            process_login(channel=channel, method=method, properties=properties, body=queue_dict)
+            self.fail('')
+        except AttributeError as e:
+            self.assertTrue(isinstance(e, (BaseException,)))
+
+        # Third case: incorrect data for json loader
+        try:
+            body = b'some incorrect data for json loader'
+            process_login(channel=channel, method=method, properties=properties, body=body)
             self.fail('')
         except ValueError as e:
             self.assertTrue(isinstance(e, (BaseException,)))
