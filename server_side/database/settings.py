77ea95aa8664b948b13c0e041dde3d4806e42d30
dzamakhaiev <dzamakhaiev@star.global>
2024-08-03 17:31:19 +0300

    fix postgres name

--- server_side/database/settings.py
+++ server_side/database/settings.py
@@ -4,7 +4,7 @@ from scripts.get_container_host import get_container_host
 INSIDE_DOCKER = int(os.environ.get('RUN_INSIDE_DOCKER', 0))
 CI_RUN = int(os.environ.get('CI_RUN', 0))
 
-CONTAINER_NAME = 'rabbitmq'
+CONTAINER_NAME = 'postgres'
 CONTAINER_NAME = CONTAINER_NAME + '-ci' if CI_RUN else CONTAINER_NAME
 
 DB_HOST = get_container_host(CONTAINER_NAME)
