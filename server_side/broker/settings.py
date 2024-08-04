1bda6602725b72793697f222a28a06383d223d12
dzamakhaiev <dzamakhaiev@star.global>
2024-08-04 16:44:06 +0300

    remove docker script

--- server_side/broker/settings.py
+++ server_side/broker/settings.py
@@ -1,15 +1,10 @@
 import os
-from scripts.get_container_info import get_container_host
 
 INSIDE_DOCKER = int(os.environ.get('RUN_INSIDE_DOCKER', 0))
 CI_RUN = int(os.environ.get('CI_RUN', 0))
 
 CONTAINER_NAME = 'rabbitmq'
-CONTAINER_NAME = CONTAINER_NAME + '-ci' if CI_RUN else CONTAINER_NAME
-
-MQ_HOST = get_container_host(CONTAINER_NAME)
-if MQ_HOST is None:
-    MQ_HOST = CONTAINER_NAME
+MQ_HOST = CONTAINER_NAME + '-ci' if CI_RUN else CONTAINER_NAME
 MQ_PORT = 5672
 CONNECT_URI = f'amqp://guest:guest@{MQ_HOST}:{MQ_PORT}/%2F'
 MQ_DELIVERY_MODE = 2  # Persistent mode saves messages on HDD in case of crash
--- server_side/database/settings.py
+++ server_side/database/settings.py
@@ -1,15 +1,10 @@
 import os
-from scripts.get_container_info import get_container_host
 
 INSIDE_DOCKER = int(os.environ.get('RUN_INSIDE_DOCKER', 0))
 CI_RUN = int(os.environ.get('CI_RUN', 0))
 
 CONTAINER_NAME = 'postgres'
-CONTAINER_NAME = CONTAINER_NAME + '-ci' if CI_RUN else CONTAINER_NAME
-
-DB_HOST = get_container_host(CONTAINER_NAME)
-if DB_HOST is None:
-    DB_HOST = CONTAINER_NAME
+DB_HOST = CONTAINER_NAME + '-ci' if CI_RUN else CONTAINER_NAME
 DB_PORT = 5432
 DB_NAME = 'postgres'
 DB_USER = 'postgres'
